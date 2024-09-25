2286ca15c4932c45ec6349b7d300b059b2138c62
matternstsr <matternstsr@gmail.com>
2024-09-24 20:00:26 -0500

    Betty

--- hash_tables/100-sorted_hash_table.c
+++ hash_tables/100-sorted_hash_table.c
@@ -136,3 +136,49 @@ void shash_table_delete(shash_table_t *ht)
 	free(ht->array);
 	free(ht);
 }
+
+/**
+* shash_table_print - Prints the sorted hash table.
+* @ht: The sorted hash table.
+*/
+void shash_table_print(const shash_table_t *ht)
+{
+	shash_node_t *current;
+
+	if (!ht)
+		return;
+
+	current = ht->shead;
+	printf("{");
+	while (current)
+	{
+		printf("'%s': '%s'", current->key, current->value);
+		if (current->snext)
+			printf(", ");
+		current = current->snext;
+	}
+	printf("}\n");
+}
+
+/**
+* shash_table_print_rev - Prints the sorted hash table in reverse.
+* @ht: The sorted hash table.
+*/
+void shash_table_print_rev(const shash_table_t *ht)
+{
+	shash_node_t *current;
+
+	if (!ht)
+		return;
+
+	current = ht->stail;
+	printf("{");
+	while (current)
+	{
+		printf("'%s': '%s'", current->key, current->value);
+		if (current->sprev)
+			printf(", ");
+		current = current->sprev;
+	}
+	printf("}\n");
+}
