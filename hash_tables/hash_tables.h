58ccbeb5b7a312637fa808c4ac6dcd18d1f0391c
matternstsr <matternstsr@gmail.com>
2024-09-24 20:03:13 -0500

    BSF

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -98,6 +98,4 @@ unsigned long int hash_djb2(const unsigned char *str); /* Proto hash func */
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 
-
-
 #endif /* HASH_TABLES_H */
