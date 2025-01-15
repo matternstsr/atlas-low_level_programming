8009d124fa959678b4ec42e143e4d512bcaf3058
matternstsr <matternstsr@gmail.com>
2025-01-15 11:56:22 -0600

    forgot to add int binary_search(int *array, size_t low, size_t high, int value);

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -6,5 +6,5 @@
 /* Function prototypes */
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
-
+int binary_search(int *array, size_t low, size_t high, int value);
 #endif /* SEARCH_ALGOS_H */
