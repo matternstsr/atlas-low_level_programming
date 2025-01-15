a09976dc411a7d5fcb703765ca006cd72dfa74d4
matternstsr <matternstsr@gmail.com>
2025-01-15 13:03:42 -0600

    corrected the print statement

--- search_algorithms/102-interpolation.c
+++ search_algorithms/102-interpolation.c
@@ -14,32 +14,28 @@
 */
 int interpolation_search(int *array, size_t size, int value)
 {
-	size_t low = 0, high = size - 1, pos;
+	size_t high = size - 1, low = 0, pos = 0;
 
-	if (array == NULL || size == 0)
+	if (!array)
 		return (-1);
-	while (low <= high && value >= array[low] && value <= array[high])
+	pos = POSITION(low, high, array, value);
+	while (low < high && value <= array[high] && value >= array[low])
 	{
-		if (array[low] == array[high]) /* Prevent division by zero */
-			break;
-		pos = low + (((double)(high - low) / (array[high] - array[low])) *
-					(value - array[low]));
-		/* Check if pos is out of range */
-		if (pos >= size)
-		{
-			printf("Value checked array[%lu] is out of range\n", pos);
-			return (-1);
-		}
-		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
+		printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
 		if (array[pos] == value)
 			return (pos);
-		if (array[pos] < value)
+		else if (array[pos] < value)
+		{
 			low = pos + 1;
-		else
-			high = pos - 1;
+			pos = POSITION(low, high, array, value);
+		}
+		else if (array[pos] > value)
+		{
+			high = pos - 1;  /* Fixed decrement */
+			pos = POSITION(low, high, array, value);
+		}
 	}
-	/* Value is not in the array */
-	if (low <= high)
-		printf("Value checked array[%lu] is out of range\n", low);
+		/* Print the correct index */
+	printf("Value checked array[%ld] is out of range\n", high);
 	return (-1);
 }
