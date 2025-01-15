8134f85b5c3bfbd3a393cbfa687f6da6da870bfc
matternstsr <matternstsr@gmail.com>
2025-01-15 12:08:58 -0600

    oops forgot a () for the return

--- search_algorithms/102-interpolation.c
+++ search_algorithms/102-interpolation.c
@@ -20,15 +20,17 @@ int interpolation_search(int *array, size_t size, int value)
 		return (-1);
 	while (low <= high && value >= array[low] && value <= array[high])
 	{
+		if (array[low] == array[high]) /* Prevent division by zero */
+			break;
 		pos = low + (((double)(high - low) / (array[high] - array[low])) *
 					(value - array[low]));
-		if (pos >= size) /* Check if pos is out of range */
+		/* Check if pos is out of range */
+		if (pos >= size)
 		{
 			printf("Value checked array[%lu] is out of range\n", pos);
 			return (-1);
 		}
 		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
-		if (array[low] == array[high]) break;
 		if (array[pos] == value)
 			return (pos);
 		if (array[pos] < value)
@@ -36,5 +38,8 @@ int interpolation_search(int *array, size_t size, int value)
 		else
 			high = pos - 1;
 	}
+	/* Value is not in the array */
+	if (low <= high)
+		printf("Value checked array[%lu] is out of range\n", low);
 	return (-1);
 }
